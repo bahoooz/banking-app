f493dc0939646369b7e1474fa209080e92dcc4a8
Bahoz <julienbraultcherif@gmail.com>
2024-06-10 15:21:26 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Bahoz", lastName: "B", email: "julienbraultcherif@gmail.com" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]} />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import { Chart as ChartJs, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJs.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+  const data = {
+    datasets: [
+      {
+        label: "Banks",
+        data: [1250, 2500, 3750],
+        backgroundColor: ["#0747b6", "#2265d8", "2f91fa"],
+      },
+    ],
+    labels: ["Bank 1", "Bank 2", "Bank 3"],
+  };
+  return (
+    <Doughnut className="max-w-24 max-h-24" width={"100%"} height={"auto"}
+      data={data}
+      options={{ cutout: "60%", plugins: { legend: { display: false } } }}
+    />
+  );
+};
+
+export default DoughnutChart;
